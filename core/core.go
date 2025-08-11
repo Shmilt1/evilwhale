7371105a8b4631657ff688d565c8634fce1a5551
Shmilt1 <askiloseberg@outlook.com>
2025-08-11 22:34:26 +0200

    updated core.go

--- core/core.go
+++ core/core.go
@@ -9,6 +9,19 @@ import (
 	"net/http"
 )
 
+func Ping(host string) bool {
+	response, err := http.Get(host + "/_ping")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	if response.StatusCode == 200 {
+		return true
+	}
+
+	return false
+}
+
 func pullImage(host, image string) {
 	response, err := http.Post(host+"/images/create?fromImage="+image, "application/json", nil)
 	if err != nil {
@@ -16,23 +29,34 @@ func pullImage(host, image string) {
 	}
 	defer response.Body.Close()
 
-	data, err := io.ReadAll(response.Body)
-	if err != nil {
-		log.Fatalln(err)
-	}
+	decoder := json.NewDecoder(response.Body)
+	for {
+		var status map[string]any
+		if err := decoder.Decode(&status); err != nil {
+			if err == io.EOF {
+				break
+			}
+			log.Fatalln(err)
+		}
 
-	log.Println(string(data))
+		j, err := json.MarshalIndent(status, "", "  ")
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		log.Println(string(j))
+	}
 }
 
 func CreateContainer(host, image string) (string, error) {
-	jsonData, err := json.Marshal(map[string]string{
+	j, err := json.Marshal(map[string]string{
 		"Image": image,
 	})
 	if err != nil {
 		log.Fatalln(err)
 	}
 
-	response, err := http.Post(host+"/containers/create", "application/json", bytes.NewBuffer(jsonData))
+	response, err := http.Post(host+"/containers/create", "application/json", bytes.NewBuffer(j))
 	if err != nil {
 		log.Fatalln(err)
 	}
@@ -45,18 +69,18 @@ func CreateContainer(host, image string) (string, error) {
 		return "", errors.New("error code " + response.Status)
 	}
 
-	respData, err := io.ReadAll(response.Body)
+	data, err := io.ReadAll(response.Body)
 	if err != nil {
 		log.Fatalln(err)
 	}
 
-	var jsonResp map[string]string
-	err = json.Unmarshal(respData, &jsonResp)
+	var m map[string]string
+	err = json.Unmarshal(data, &m)
 	if err != nil {
 		log.Fatalln(err)
 	}
 
-	return jsonResp["Id"], nil
+	return m["Id"], nil
 }
 
 func Exec(host, id string) {
